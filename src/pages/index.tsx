ab1645dd66c652183ef8e57276f2193af4d9d6b0
Luan Novais <luaannovais@live.com>
2021-04-20 17:29:02 -0300

    Feat: ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,15 @@ export default function Home() {
     <h1>Index</h1>
   )
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
